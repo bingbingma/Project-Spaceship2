43b5f20f9da73b430178b3db1e5315302df7012a
bingbingma <7047982+bingbingma@users.noreply.github.com>
2019-05-02 21:33:41 -0400

    new jaws if

--- config/connection.js
+++ config/connection.js
@@ -30,16 +30,22 @@ var mysql = require("mysql");
 // connection = mysql.createConnection(process.env.JAWSDB_URL);
 //   var sequelize = new Sequelize(process.env.JAWSDB_URL);
 // } else {
-var sequelize = new Sequelize("messages_db", "root", "", {
-  host: "localhost",
-  port: 8889,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+
+if (process.env.JAWSDB_URL) {
+  // connection = mysql.createConnection(process.env.JAWSDB_URL);
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  var sequelize = new Sequelize("messages_db", "root", "", {
+    host: "localhost",
+    port: 8889,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+}
 // }
 // Exports the connection for other files to use
 module.exports = sequelize;
